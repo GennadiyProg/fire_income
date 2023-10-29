f0613faac8739cf7895a4ac700e6135f19bb410d
rumjancev-gs-220525 <rumjancev-gs-220525@fsvps.ru>
2023-10-29 13:35:25 +0300

    Initial commit

---
+++ android/app/src/main/java/com/example/fire_income/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.fire_income;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/models/Product.dart
@@ -0,0 +1,25 @@
+class Product{
+
+  String? name;
+  String? category;
+  String? measureUnit;
+
+
+  Product.empty();
+
+  Product({this.name, this.category, this.measureUnit});
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+      name: json['name'],
+      category: json['category'],
+      measureUnit: json['measureUnit'],
+  );
+
+  Map<String, dynamic> toJson() => <String, dynamic>{
+    'name': name,
+    'category': category,
+    'measureUnit': measureUnit,
+  };
+
+
+}
